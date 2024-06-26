ab3119a0835fac10b2abcacd0abc66b4f496acfc
Braden Gerlach <139206279+ThatOneTaxCollector@users.noreply.github.com>
2024-06-26 06:38:29 +0000

    Add files via upload

---
+++ Lab 3/Lab/Hello world git-copy/HelloWorld.java
@@ -0,0 +1,27 @@
+
+/**
+ * This is a basic Hello World intro script.
+ *
+ * @author braden gerlach
+ * @version 5.14.2024
+ */
+public class HelloWorld
+{
+    /**
+     * Says Hello world and Hello John
+     */
+    public HelloWorld()
+    {
+        // initialise instance variables
+        System.out.println("Hello, World!\n"+"Hello John");
+    }
+    /**
+     * Says Hello to the @param name var
+     * @param name
+     */
+    public void sayHelloToName(String name)
+    {
+        System.out.println("Hello "+ name + "!");
+    }
+
+}
